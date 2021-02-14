11966555aa8945cd05e419f8346c62dc87fef45e
Jeong <qsz78547@naver.com>
2021-02-14 12:57:19 +0900

    만들어보는 월급계산기 1일

---
+++ app/src/main/java/com/example/calender/fragment/LoginFragment.kt
@@ -0,0 +1,24 @@
+package com.example.calender.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.calender.R
+
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+
+
+}
