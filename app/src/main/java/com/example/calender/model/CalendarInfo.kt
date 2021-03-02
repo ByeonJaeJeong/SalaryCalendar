dfe9b46d843450ed33679bd41d41c3b134805b8e
Jeong <qsz78547@naver.com>
2021-03-02 22:53:52 +0900

    text model 추가

--- app/src/main/java/com/example/calender/model/CalendarInfo.kt
+++ app/src/main/java/com/example/calender/model/CalendarInfo.kt
@@ -1,7 +1,13 @@
 package com.example.calender.model
 
+import java.time.Year
+
 data class CalendarInfo(
+    //캘린더 출력 날짜
+    var year: Int=0,
     var month : Int= 0,
     var dayOfMonth: Int =0,
-    var dayOfWeek : Int =0
+    var dayOfWeek : Int =0,
+    var workInfo: WorkInfo
+
 )
---
+++ app/src/main/java/com/example/calender/model/WorkInfo.kt
@@ -0,0 +1,10 @@
+package com.example.calender.model
+
+data class WorkInfo(
+        // 일한정보
+        var workPlace :String,
+        var workDay: Int,
+        var color: String,
+        var workType: String,
+        var workTime: Int
+)
